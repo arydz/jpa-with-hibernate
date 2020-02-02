a90a48b465103001e09edb1e31f6ef7956b30d60
arydz <arydz@sii.pl>
2020-02-02 22:58:08 +0100

    Initial commit

---
+++ src/test/java/com/arydz/hibernate/learning/jpawithhibernate/JpaWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arydz.hibernate.learning.jpawithhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
