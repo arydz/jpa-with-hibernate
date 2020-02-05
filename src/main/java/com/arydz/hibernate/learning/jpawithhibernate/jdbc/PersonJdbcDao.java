0cf1b075c68a70bc3ce8d2ab77bbc1fc328d9f2c
arydz <arydz@sii.pl>
2020-02-05 22:58:24 +0100

    Implemented custom row mapper for person model

--- src/main/java/com/arydz/hibernate/learning/jpawithhibernate/jdbc/PersonJdbcDao.java
+++ src/main/java/com/arydz/hibernate/learning/jpawithhibernate/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.arydz.hibernate.learning.jpawithhibernate.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -16,9 +19,23 @@ public class PersonJdbcDao {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	// We implements custom row mappers only in situations where table definition is different then your model. This is only trivial example.
+	static class PersonRowMapper implements RowMapper<Person> {
+
+		@Override
+		public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+			Person person = new Person();
+			person.setId(resultSet.getInt("id"));
+			person.setName(resultSet.getString("name"));
+			person.setCountry(resultSet.getString("country"));
+			person.setBirthDate(resultSet.getTimestamp("birth_date"));
+			return person;
+		}
+	}
+
 	public List<Person> findAll() {
 		// BeanPropertyRowMapper it's default row mapper which map every row on appropriate java object
-		return jdbcTemplate.query("SELECT * FROM PERSON", new BeanPropertyRowMapper<>(Person.class));
+		return jdbcTemplate.query("SELECT * FROM PERSON", new PersonRowMapper());
 	}
 
 	public Person findById(int id) {
