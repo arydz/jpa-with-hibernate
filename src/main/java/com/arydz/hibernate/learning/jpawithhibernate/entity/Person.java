8038bbef34f356995abd2806eab2c5ae3e52a1f4
arydz <arydz@sii.pl>
2020-02-06 19:14:28 +0100

    Defining person entity

--- src/main/java/com/arydz/hibernate/learning/jpawithhibernate/entity/Person.java
+++ src/main/java/com/arydz/hibernate/learning/jpawithhibernate/entity/Person.java
@@ -7,24 +7,37 @@
  */
 package com.arydz.hibernate.learning.jpawithhibernate.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Entity
+//There is no need to use this name mapping, because class name matches sql table
+//@Table(name = "person")
 public class Person {
 
+	@Id	// Primary key
+	@GeneratedValue	// this annotation populate id value - hibernate create sequence in DB which generates next int values
 	private Integer id;
+
+	// It's also unnecessary annotation in this case
+//	@Column(name = "name")
 	private String name;
 	private String country;
 	private Date birthDate;
 
-	// BeanPropertyRow mapper require non-args constructor!
-	public Person() {
+	// Hibernate require constructor without id
+	public Person(String name, String country, Date birthDate) {
+		super();
+		this.name = name;
+		this.country = country;
+		this.birthDate = birthDate;
 	}
 
 	@Override
